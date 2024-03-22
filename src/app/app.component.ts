d756aeb93e03967c9fbbfd4a19574a44264f125f
Jordan Pang <jordan.pang@rmaict.com>
2024-03-22 11:28:13 +0800

    Added login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [RouterOutlet, LoginComponent],
+  template: `
+    <h1>
+      {{ title }}
+    </h1>
+    <app-login></app-login>
+  `,
 })
 export class AppComponent {
-  title = 'Home';
+  title = 'Login Page';
 }
