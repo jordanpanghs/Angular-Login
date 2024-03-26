63e323f6fdab9f571e1a265bb331a10b8930a542
Jordan Pang <jordan.pang@rmaict.com>
2024-03-26 09:45:04 +0800

    Linked API to backend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import {
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,9 @@ import {
   standalone: true,
   template: `
     <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
+      <button class="switch-button" (click)="switchForm()">
+        {{ isLoginForm ? 'Switch to Register' : 'Switch to Login' }}
+      </button>
       <div class="input-field">
         <label>Username</label>
         <input type="text" formControlName="username" />
@@ -20,12 +24,16 @@ import {
         <label>Password</label>
         <input type="password" formControlName="password" />
       </div>
-      <button type="submit">Login</button>
+      <button type="submit">{{ isLoginForm ? 'Login' : 'Register' }}</button>
     </form>
   `,
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private http: HttpClient) {} // Inject HttpClient
+
+  isLoginForm = true;
+
   loginForm = new FormGroup({
     username: new FormControl(null, Validators.required),
     password: new FormControl(null, Validators.required),
@@ -38,7 +46,25 @@ export class LoginComponent {
     });
   }
 
+  switchForm() {
+    this.isLoginForm = !this.isLoginForm;
+  }
+
   onSubmit() {
-    console.log(this.loginForm.value);
+    if (this.loginForm.valid) {
+      const url = this.isLoginForm
+        ? 'http://localhost:5065/api/Auth/login'
+        : 'http://localhost:5065/api/Auth/register';
+      this.http.post(url, this.loginForm.value).subscribe(
+        (response) => {
+          console.log(response);
+          // Handle successful response
+        },
+        (error) => {
+          console.log(error);
+          // Handle error response
+        }
+      );
+    }
   }
 }
